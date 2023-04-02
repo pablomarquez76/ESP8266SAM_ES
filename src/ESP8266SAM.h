52ccfed7c1f2d3e44a059c453d2aba058c5ba7b7
pablomarquez76 <92705318+pablomarquez76@users.noreply.github.com>
2023-04-02 10:42:27 -0300

    Update ESP8266SAM.h

--- src/ESP8266SAM.h
+++ src/ESP8266SAM.h
@@ -1,8 +1,9 @@
 /*
   ESP8266SAM
   Port of SAM to the ESP8266
-  
+
   Copyright (C) 2017  Earle F. Philhower, III
+  Modified by Juan Pablo Marquez
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -14,57 +15,100 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
   
-  added: Regional accents 
+  added: Regional accents
 */
 
-#ifndef _ESP8266SAM_H
-#define _ESP8266SAM_H
 
 #include <Arduino.h>
-#include <AudioOutput.h>
+#include <ESP8266SAM.h>
 
-class ESP8266SAM {
+#include "reciter.h"
+#include "sam.h"
+#include "SamData.h"
 
-public:
-  ESP8266SAM()
+SamData* samdata;
+
+// Thunk from C to C++ with a this-> pointer
+void ESP8266SAM::OutputByteCallback(void *cbdata, unsigned char b)
+{
+  ESP8266SAM *sam = static_cast<ESP8266SAM*>(cbdata);
+  sam->OutputByte(b);
+}
+
+void ESP8266SAM::OutputByte(unsigned char b)
+{
+  // Xvert unsigned 8 to signed 16...
+  int16_t s16 = b;// s16 -= 128; //s16 *= 128;
+  int16_t sample[2];
+  sample[0] = s16;
+  sample[1] = s16;
+  while (!output->ConsumeSample(sample)) yield();
+}
+
+bool ESP8266SAM::Say(AudioOutput *out, const char *str)
+{
+  if (!str || strlen(str) > 254) return false; // Only can speak up to 1 page worth of data...
+  samdata = new SamData;
+  if (samdata == nullptr)
   {
-    singmode = false;
-    phonetic = false;
-    pitch = 0;
-    mouth = 0;
-    throat = 0;
-    speed = 0;
-    output = NULL;
-  };
-  
-  ~ESP8266SAM()
-  {
+    // allocation failed!
+    return false;
   }
 
-  enum SAMVoice { VOICE_SAM, VOICE_ELF, VOICE_ROBOT, VOICE_STUFFY, VOICE_OLDLADY, VOICE_ET };
-  enum SAMRegion { REGION_AR, REGION_ES, REGION_OT };
-  void SetVoice(enum SAMVoice voice);
-  void SetRegion(enum SAMRegion region);
+  // These are fixed by the synthesis routines
+  out->SetRate(22050);
+  out->SetBitsPerSample(8);
+  out->SetChannels(1);
+  out->begin();
 
-  void SetSingMode(bool val) { singmode = val; }
-  void SetPhonetic(bool val) { phonetic = val; }
-  void SetPitch(uint8_t val) { pitch = val; }
-  void SetMouth(uint8_t val) { mouth = val; }
-  void SetThroat(uint8_t val) { throat = val; }
-  void SetSpeed(uint8_t val) { speed = val; }
+  // SAM settings
+  EnableSingmode(singmode);
+  if (speed) ::SetSpeed(speed);
+  if (pitch) ::SetPitch(pitch);
+  if (mouth) ::SetMouth(mouth);
+  if (throat) ::SetThroat(throat);
 
-  bool Say(AudioOutput *out, const char *str);
+  char input[256];
+  if (phonetic) {
+    for (int i = 0; str[i]; i++)
+      input[i] = toupper((int)str[i]);
+    input[strlen(str)] = 0;
+  } else {
+    char texto[60];
+    for (int i = 0; str[i]; i++)
+      texto[i] = str[i];
+    texto[strlen(str)] = 0;
+    textoAFonemas(texto, input);
+  }
+  strncat(input, "\x9b", 255);
 
-private:
-  static void OutputByteCallback(void *cbdata, unsigned char b);
-  void OutputByte(unsigned char b);
-  bool singmode;
-  bool phonetic;
-  int pitch;
-  int speed;
-  int mouth;
-  int throat;
-  AudioOutput *output;
-};
+  // Say it!
+  output = out;
+  SetInput(input);
+  SAMMain(OutputByteCallback, (void*)this);
+  delete samdata;
+  return true;
+}
 
-#endif
+void ESP8266SAM::SetVoice(enum SAMVoice voice)
+{
+  switch (voice) {
+    case VOICE_ELF: SetSpeed(72); SetPitch(64); SetThroat(110); SetMouth(160); break;
+    case VOICE_ROBOT: SetSpeed(92); SetPitch(60); SetThroat(190); SetMouth(190); break;
+    case VOICE_STUFFY: SetSpeed(82); SetPitch(72); SetThroat(110); SetMouth(105); break;
+    case VOICE_OLDLADY: SetSpeed(82); SetPitch(32); SetThroat(145); SetMouth(145); break;
+    case VOICE_ET: SetSpeed(100); SetPitch(64); SetThroat(150); SetMouth(200); break;
+    default:
+    case VOICE_SAM: SetSpeed(72); SetPitch(64); SetThroat(128); SetMouth(128); break;
+  }
+}
+
+void ESP8266SAM::SetRegion(enum SAMRegion region)
+{
+  switch (region) {
+    case REGION_AR: regionN = 0; break;
+    case REGION_ES: regionN = 1; break;
+    default:
+    case REGION_OT: regionN = 2; break;
+  }
+}
